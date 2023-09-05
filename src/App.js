feb7ed7deed0f975e40f127391a1bc2148b0c9ec
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-09-05 20:15:06 -0300

    Modifiqué código en App para mejor visualización

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con react.
-          {/* jo */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <>           
+            <h1>Tareas Pendientes de Hedy Lamarr</h1>
+            <img
+              src="https://i.imgur.com/yXOvdOSs.jpg"
+              alt="Hedy Lamarr"
+              classname="photo"
+            />
+            <ul>
+              <li>Inventar nuevo semáforo</li>
+              <li>Ensayar la escena de la película</li>
+              <li>Mejorar la tecnología del espectro</li>
+            </ul>
+            ;
+        </>
+
       </header>
     </div>
   );
